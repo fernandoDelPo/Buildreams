38e4a229f69284b4fc1811cfda660ba4ebaec6bf
MatyDje <matiasmu1705@gmail.com>
2022-09-19 18:24:17 -0300

    pagina de login  sin header ni footer

--- app.js
+++ app.js
@@ -14,6 +14,18 @@ app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './src/views/index.html'));
 });
 
-app.get('/register.html', (req, res) => {
+app.get('/register', (req, res) => {
     res.sendFile(path.resolve(__dirname, './src/views/register.html'));
+});
+
+app.get('/login', (req,res)=>{
+    res.sendFile(path.resolve(__dirname, './src/views/login.html'))
+});
+
+app.get('/productCart', (req,res)=>{
+    res.sendFile(path.resolve(__dirname, './src/views/productCart.html'))
+});
+
+app.get('/productDetail', (req,res)=>{
+    res.sendFile(path.resolve(__dirname, './src/views/productDetail.html'))
 });
