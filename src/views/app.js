5930f64aee2e1c1acb0be039ed5fc9f7f1f005b1
MatyDje <matiasmu1705@gmail.com>
2022-10-05 15:18:53 -0300

    arreglos del start

--- src/views/app.js
+++ src/views/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const main = require('./router/main');
+const main = require('../router/main');
 
 app.use(express.static(path.resolve(__dirname, "../public")))
 app.use('/', main);
