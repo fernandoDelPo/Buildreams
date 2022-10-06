7e4c642b7d277a18615823b51badb50dcd8214a2
fernandoDelpo <fernandodelpozzi@gmail.com>
2022-10-06 20:57:19 -0300

    agregando boton eliminar y modificar en productDetail

--- src/controllers/productcontroller.js
+++ src/controllers/productcontroller.js
@@ -5,20 +5,27 @@ const productsFilePath = path.join(__dirname, '../data/productsDB.json');
 const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
 const productController = {
-    productCart: (req, res) => {
-       res.render('productCart', {})
-    },
-    productDetail: (req, res) => {
-       let idproduct = req.params.id;
-       let product = products.find(product => product.id == idproduct);
-       res.render('productDetail', { product, products });
-    },
-    create: (req, res) => {
-		res.render('createProducts')
-		// console.log(req.cookies.color);
+   productCart: (req, res) => {
+      res.render('productCart', {})
+   },
+   productDetail: (req, res) => {
+      let idproduct = req.params.id;
+      let product = products.find(product => product.id == idproduct);
+      res.render('productDetail', {
+         product,
+         products
+      });
+   },
+   create: (req, res) => {
+      res.render('createProducts')
+      // console.log(req.cookies.color);
+   },
+   edit: (req, res) => {
+		let idProduct = req.params.id
+		let productEdit = products.find(product => product.id == idProduct)
+		res.render('editProduct', {productEdit})
 	},
 
+}
 
- }
- 
- module.exports = productController;
+module.exports = productController;
