0504f8b105bd73af5587868276a2988e8a7b6ab0
egiampieri <eve.giampieri@gmail.com>
2022-11-11 20:57:24 -0300

    default

--- src/controllers/usercontroller.js
+++ src/controllers/usercontroller.js
@@ -4,6 +4,8 @@ const path = require('path');
 const userFilePath = path.join(__dirname, '../data/userDB.json');
 const users = JSON.parse(fs.readFileSync(userFilePath, 'utf-8'));
 
+const userLoginInfoFilePath = path.join(__dirname, '../data/userLoginInfo.json');
+const usersLoginInfo = JSON.parse(fs.readFileSync(userLoginInfoFilePath, 'utf-8'));
 
 const userController = {
     register: (req, res) => {
@@ -15,8 +17,8 @@ const userController = {
     profile: (req, res) => {
 		res.render('profile')
     },
-    
     store:  (req, res) => {
+        
 	},
 
     logout: (req,res) => {
