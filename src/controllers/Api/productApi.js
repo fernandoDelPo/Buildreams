395a1c5752cfc1356c49232d614e2097657ccb31
fernandoDelpo <fernandodelpozzi@gmail.com>
2023-01-12 22:30:59 -0300

    carpeta front-react

---
+++ src/controllers/Api/productApi.js
@@ -0,0 +1,143 @@
+let db = require("../../database/models");
+const Op = db.Sequelize.Op;
+
+const productApi = {
+  products: (req, res) => {
+    db.Products.findAll()
+      .then((products) => {
+        let countTotal = 0;
+        let countByCategory = 1;
+        /* Contador de productos por categoria */
+        for (let i = 1; i < products.length; i++) {
+          if (products[i].category == "Women´s") {
+            countByCategory += 1;
+          } else {
+            countTotal += 1;
+          }
+        }
+        /* Imprime campo detail en producto con url api */
+        for (let i = 0; i < products.length; i++) {
+          products[i].setDataValue(
+            "detail",
+            `http://localhost:${process.env.PORT}/api/products/${products[i].id}`
+          );
+        }
+
+        /* Imprime url de la foto para consumir */
+        for (let i = 0; i < products.length; i++) {
+          products[i].setDataValue(
+            "pathImg",
+            `http://localhost:${process.env.PORT}/images/shoes-img/${products[i].productName}/${products[i].img}`
+          );
+        }
+
+        let response = {
+          count: products.length,
+          countByCategory: [
+            {
+              men: countMens,
+            },
+            {
+              women: countWomens,
+            },
+          ],
+          data: products,
+          status: 200,
+        };
+
+        res.status(200).json(response);
+      })
+      .catch((error) => res.json(error));
+  },
+
+  productDetail: (req, res) => {
+    db.Product.findByPk(req.params.id)
+      .then(function (productSelected) {
+        let response = {
+          id: productSelected.id,
+          productName: productSelected.productName,
+          brand: productSelected.brand,
+          description: productSelected.description,
+          color: productSelected.color,
+          price: productSelected.price,
+          size: productSelected.size,
+          pathImg: `http://localhost:${process.env.PORT}/images/shoes-img/${productSelected.productName}/${productSelected.img}`,
+          status: 200,
+        };
+
+        res.status(200).json(response);
+      })
+      .catch((error) => res.json(error));
+  },
+
+  store: (req, res) => {
+    db.Product.create(req.body)
+      .then((product) => {
+        let response = {
+          data: product,
+          status: 200,
+          created: "ok",
+        };
+        return res.status(200).json(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
+
+  delete: (req, res) => {
+    db.Product.destroy({
+      where: { id: parseInt(req.params.id, 10) },
+    })
+      .then(() => {
+        return res.json("Product Deleted");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
+
+  search: (req, res) => {
+    db.Product.findAll({
+      where: {
+        productName: { [Op.like]: "%" + req.query.keyword + "%" },
+      },
+    })
+      .then((products) => {
+        if (products.length > 0) {
+          /* Imprime url de la foto para consumir */
+          for (let i = 0; i < products.length; i++) {
+            products[i].setDataValue(
+              "pathImg",
+              `http://localhost:${process.env.PORT}/images/shoes-img/${products[i].productName}/${products[i].img}`
+            );
+          }
+          res.status(200).json({
+            data: products,
+            status: 200,
+          });
+        } else {
+          res.status(200).json({
+            data: "No existen productos con ese nombre",
+            status: 200,
+          });
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
+
+  checkout: async function (req, res) {
+    let order = await db.Order.create(
+      {
+        ...req.body,
+        userId: req.session.userLogged.id,
+      },
+      { include: db.Order.OrderItems }
+    );
+    res.json({ ok: true, status: 200, order: order });
+  },
+};
+
+module.exports = productApi;
---
+++ src/controllers/Api/userApi.js
@@ -0,0 +1,46 @@
+const fs = require("fs");
+const { validationResult } = require("express-validator");
+const bcryptjs = require("bcryptjs");
+const db = require("../../database/models");
+
+const userApi = {
+  allUsers: (req, res) => {
+    db.User.findAll({
+      attributes: ["id", "fullName", "email"],
+    })
+      .then((users) => {
+        for (let i = 0; i < users.length; i++) {
+          users[i].setDataValue(
+            "detail",
+            `http://localhost:${process.env.PORT}/api/users/profile/${users[i].id}`
+          );
+        }
+
+        let response = {
+          count: users.length,
+          users: users,
+          status: 200,
+        };
+
+        res.status(200).json(response);
+      })
+      .catch((error) => res.json(error));
+  },
+
+  profile: (req, res) => {
+    db.User.findByPk(parseInt(req.params.id, 10))
+      .then((user) => {
+        let response = {
+          id: user.id,
+          name: user.fullName,
+          username: user.userName,
+          email: user.email,
+          country: user.country,
+          avatar: `http://localhost:${process.env.PORT}/images/avatars/${user.avatar}`,
+        };
+        res.status(200).json(response);
+      })
+      .catch((error) => console.error(error));
+  },
+};
+module.exports = userApi;
